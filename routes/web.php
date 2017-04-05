603de8921054bfcc8a7425ab57a4c952d048f66b
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2017-04-05 17:14:49 -0500

    multiple results

--- routes/web.php
+++ routes/web.php
@@ -21,39 +21,45 @@ $app->post('/{bot}', function (Illuminate\Http\Request $request, $bot) use ($app
     Log::debug("request to {$request->url()}", $request->all());
 
     if ($request->input('inline_query')) {
+        $results = [];
+
+        for ($i=0; $i<5; $i++) {
+            $id = uniqid();
+
+            $results[] = [
+                'type' => 'photo',
+                'id' => $id,
+                'photo_url' => 'https://tbots.categulario.tk/mountain.png',
+                'thumb_url' => 'https://tbots.categulario.tk/mountain.png',
+                'title' => $id,
+                'description' => 'Esta es la descripción',
+                'caption' => 'Forecast for Cofre de perote',
+
+                'reply_markup' => [
+                    'inline_keyboard' => [
+                        [
+                            'text' => 'foo',
+                        ],
+                        [
+                            'text' => 'var',
+                        ],
+                    ],
+                ],
+
+                'input_message_content' => [
+                    'message_text' => 'message text',
+                    'parse_mode' => 'HTML',
+                    'disable_web_page_preview' => true,
+                ],
+            ];
+        }
+
         return [
             'method' => 'answerInlineQuery',
 
             'inline_query_id' => $request->input('inline_query.id'),
             'cache_time' => 0,
-            'results' => [
-                [
-                    'type' => 'photo',
-                    'id' => uniqid(),
-                    'photo_url' => 'https://tbots.categulario.tk/mountain.png',
-                    'thumb_url' => 'https://tbots.categulario.tk/mountain.png',
-                    'title' => uniqid(),
-                    'description' => 'Esta es la descripción',
-                    'caption' => 'Forecast for Cofre de perote',
-
-                    'reply_markup' => [
-                        'inline_keyboard' => [
-                            [
-                                'text' => 'foo',
-                            ],
-                            [
-                                'text' => 'var',
-                            ],
-                        ],
-                    ],
-
-                    'input_message_content' => [
-                        'message_text' => 'message text',
-                        'parse_mode' => 'HTML',
-                        'disable_web_page_preview' => true,
-                    ],
-                ],
-            ],
+            'results' => $results,
         ];
     }
 
